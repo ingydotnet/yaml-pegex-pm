9f62b7440aac38ceb22ca46614d3937ad70f8f17
Ingy döt Net <ingy@ingy.net>
2014-08-20 22:26:28 -0700

    Small grammar change

--- lib/YAML/Pegex/Grammar.pm
+++ lib/YAML/Pegex/Grammar.pm
@@ -61,7 +61,7 @@ sub rule_block_undent {
 #     Pegex::Bootstrap->new->compile($grammar)->tree;
 # }
 # sub make_treeXXX {
-sub make_tree {
+sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.54)
   {
     '+grammar' => 'yaml',
     '+toprule' => 'yaml_stream',
@@ -73,7 +73,7 @@ sub make_tree {
       '.rgx' => qr/\G\ /
     },
     'block_key' => {
-      '.rgx' => qr/\G(\|\r?\nXXX|\>\r?\nXXX|"[^"]*"|'[^']*'|(?![&\*\#\{\}\[\]%`\@]).+?(?=:\s|\r?\n|\z)):(?:\ +|\ *(?=\r?\n))/
+      '.rgx' => qr/\G(\|\r?\nXXX|\>\r?\nXXX|"[^"]*"|'[^']*'|(?![&\*\#\{\}\[\]%`\@]).+?(?=:\s|\r?\n|\z))(?=:(?:\ +|\ *(?=\r?\n)))/
     },
     'block_mapping' => {
       '.all' => [
@@ -98,9 +98,15 @@ sub make_tree {
           '.ref' => 'block_key'
         },
         {
+          '.ref' => 'block_mapping_separator'
+        },
+        {
           '.ref' => 'block_value'
         }
       ]
+    },
+    'block_mapping_separator' => {
+      '.rgx' => qr/\G:(?:\ +|\ *(?=\r?\n))/
     },
     'block_node' => {
       '.any' => [
